89784f8859407fb6c76ee6f06d64790a0e029f72
RudyD <duriez.ry@gmail.com>
2022-08-30 06:38:05 +0200

    Accès BDD

--- core/src/main/java/org/maCompagnie/facturation/service/FactureServiceGarage.java
+++ core/src/main/java/org/maCompagnie/facturation/service/FactureServiceGarage.java
@@ -2,14 +2,15 @@ package org.maCompagnie.facturation.service;
 
 import org.maCompagnie.facturation.model.Facture;
 import org.maCompagnie.facturation.repository.FactureRepository;
+import org.maCompagnie.facturation.repository.FactureRepositoryGarage;
 
 public class FactureServiceGarage {
     private static long dernierNumFacture = 112L;
 
     //Les numéros de factures du garage ont comme forme FACT_112
-    private FactureRepository factureRepository = new FactureRepository();
+    private FactureRepositoryGarage  factureRepositoryGarage = new FactureRepositoryGarage();
     public void creerFacture (Facture facture){
         facture.setNumero(String.valueOf("FACT_" + (++dernierNumFacture)));
-        factureRepository.creer(facture);
+        factureRepositoryGarage.creer(facture);
     }
 }
