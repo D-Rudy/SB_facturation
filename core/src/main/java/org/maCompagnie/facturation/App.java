e6f62a5ac023f7a5693fb730e12ee78d7ccbe766
RudyD <duriez.ry@gmail.com>
2022-08-31 18:43:05 +0200

    conteneur léger

--- core/src/main/java/org/maCompagnie/facturation/App.java
+++ core/src/main/java/org/maCompagnie/facturation/App.java
@@ -17,9 +17,10 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-
+//
         ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
         FactureControllerInterface factureController = context.getBean(FactureControllerInterface.class);
+        //Instancie l'interface pour pouvoir changé d'implémentation  plus simplement
         factureController.creerFacture();
     }
 }
