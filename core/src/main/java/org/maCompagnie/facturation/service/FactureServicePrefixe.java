360f20a0e43a2f44b072f809bf969d6540f5c79b
RudyD <duriez.ry@gmail.com>
2022-08-31 19:01:25 +0200

    Affecter une valeur a une propriété

--- core/src/main/java/org/maCompagnie/facturation/service/FactureServicePrefixe.java
+++ core/src/main/java/org/maCompagnie/facturation/service/FactureServicePrefixe.java
@@ -3,23 +3,20 @@ package org.maCompagnie.facturation.service;
 import org.maCompagnie.facturation.model.Facture;
 import org.maCompagnie.facturation.repository.FactureRepositoryInterface;
 
+import java.io.File;
+
 public class FactureServicePrefixe implements FactureServiceInterface{
-    private static long dernierNumFacture = 112L;
+    private long dernierNumFacture = 112L;
+    private String prefix;
+    private File fichier;
 
     //Les numéros de factures du garage ont comme forme FACT_112
     private FactureRepositoryInterface factureRepository;//On n'instancie pas de classe concrète pour éviter les dépendances
     public void creerFacture (Facture facture){
-        facture.setNumero(String.valueOf("FACT_" + (++dernierNumFacture)));
+        facture.setNumero(String.valueOf(prefix + (++dernierNumFacture)));
         factureRepository.creer(facture);
     }
 
-    public static long getDernierNumFacture() {
-        return dernierNumFacture;
-    }
-
-    public static void setDernierNumFacture(long dernierNumFacture) {
-        FactureServicePrefixe.dernierNumFacture = dernierNumFacture;
-    }
 
     public FactureRepositoryInterface getFactureRepository() {
         return factureRepository;
@@ -27,5 +24,29 @@ public class FactureServicePrefixe implements FactureServiceInterface{
 
     public void setFactureRepository(FactureRepositoryInterface factureRepository) {
         this.factureRepository = factureRepository;
+    }
+
+    public long getDernierNumFacture() {
+        return dernierNumFacture;
+    }
+
+    public void setDernierNumFacture(long dernierNumFacture) {
+        this.dernierNumFacture = dernierNumFacture;
+    }
+
+    public String getPrefix() {
+        return prefix;
+    }
+
+    public void setPrefix(String prefix) {
+        this.prefix = prefix;
+    }
+
+    public File getFichier() {
+        return fichier;
+    }
+
+    public void setFichier(File fichier) {
+        this.fichier = fichier;
     }
 }
