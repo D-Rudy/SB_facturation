2eee6aea4b963ad624311824594ba4e3fa3865c8
RudyD <duriez.ry@gmail.com>
2022-09-01 14:06:23 +0200

    classes de configuration

--- core/src/main/java/org/maCompagnie/facturation/App.java
+++ core/src/main/java/org/maCompagnie/facturation/App.java
@@ -1,18 +1,24 @@
 package org.maCompagnie.facturation;
 
 import org.maCompagnie.facturation.controller.FactureControllerInterface;
+import org.maCompagnie.facturation.service.FactureServiceInterface;
+import org.maCompagnie.facturation.service.prefixe.FactureServicePrefixe;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.*;
 
-
+@Configuration
+@ComponentScan(basePackages = {"org.maCompagnie.facturation.controller.web" /*, "org.maCompagnie.facturation.service.prefixe"*/, "org.maCompagnie.facturation.repository.database"})
+@PropertySource("classpath:application.properties")
 public class App {
     public static void main(String[] args) {
 
-        //ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        ApplicationContext context = new AnnotationConfigApplicationContext(App.class);
         FactureControllerInterface factureController = context.getBean(FactureControllerInterface.class);
         //Instancie l'interface pour pouvoir changé d'implémentation  plus simplement
         factureController.creerFacture();
+    }
+    @Bean
+    public FactureServiceInterface configureFactureService(){
+        return new FactureServicePrefixe();
     }
 }
