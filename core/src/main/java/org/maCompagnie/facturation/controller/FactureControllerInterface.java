d4440f29ba70f25e265b4c2eadcf1c9c593120e3
D-Rudy <duriez.ry@gmail.com>
2022-10-21 16:44:45 +0200

    test

--- core/src/main/java/org/maCompagnie/facturation/controller/FactureControllerInterface.java
+++
@@ -1,8 +0,0 @@
-package org.maCompagnie.facturation.controller;
-
-import org.maCompagnie.facturation.service.FactureServiceInterface;
-
-public interface FactureControllerInterface {
-    void creerFacture();
-    void setFactureService(FactureServiceInterface factureService);
-}
